7326fa78198bb8ffbd580d64c900e2d57d2a49c7
rrupom <rakib.t.rupom@gmail.com>
2023-12-15 00:43:20 +0600

    feat: modified

--- Basic Data Structures/LinkedList/InsertAtBeginLL.java
+++ Basic Data Structures/LinkedList/InsertAtBeginLL.java
@@ -20,6 +20,7 @@ public class InsertAtBeginLL {
     public static void printLL(Node head) {
         while (head != null) {
             System.out.print(head.data + " ");
+            head = head.next;
         }
         System.out.println("");
     }
