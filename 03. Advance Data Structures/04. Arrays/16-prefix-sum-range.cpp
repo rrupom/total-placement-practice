233437951719e0ed322f2962456a31315776c7ab
rrupom <rakib.t.rupom@gmail.com>
2024-07-23 20:29:20 +0600

    feat: unsolved problems and question added

---
+++ 03. Advance Data Structures/04. Arrays/16-prefix-sum-range.cpp
@@ -0,0 +1,12 @@
+/*
+	Problem : Given n ranges, find the maximum appearing elements in that range
+
+	I/P: L[] = {1, 2, 5, 15}
+		 R[] = {5, 8, 7, 18}
+	So ranges are = {1, 5} => {1, 2, 3, 4 ,5}
+	 				{2, 8} => {2, 3, 4, 5, 6, 7, 8}
+	 				{5, 7} => {5, 6, 7}
+	 				{15, 18} => {15, 16, 17, 18}
+	O/P: 5
+
+*/
---
+++ 03. Advance Data Structures/04. Arrays/prefix-sum-extra-questions.md
@@ -0,0 +1,5 @@
+## Practice problems on prefix sum
+
+- Check if a given array can be divided into three parts with equal sum
+- Check if there is a subarray with 0 sum
+- Find the longest subarray with equal 0's and 1's
